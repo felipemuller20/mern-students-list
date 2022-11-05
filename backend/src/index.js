5a6547c79bc719de910104dca6b73c5893e76cc4
felipe <felipeavmuller@gmail.com>
2022-11-05 01:38:18 -0300

    create mongo server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 const PORT = 3001;
+
+mongoose.connect(process.env.MONGO_DB_URL || 'mongodb://localhost:27017/StudentsList', {
+  useNewUrlParser: true,
+})
 
 app.use(cors());
 app.use(bodyParser.json());
